84eac4c6b404f0b8f0ef317b683fcb57cc4a383b
LAB-USR-LCENTRO <LAB-USR-LCENTRO@grupoutp.net>
2026-06-17 09:49:32 -0500

    feat: añadir función leerNota con validación do-while para V2

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int nota1, nota2, nota3, promedio;
 
-        System.out.print("Ingrese nota 1: ");
-        nota1 = sc.nextInt();
-        
-        System.out.print("Ingrese nota 2: ");
-        nota2 = sc.nextInt();
-        
-        System.out.print("Ingrese nota 3: ");
-        nota3 = sc.nextInt();
+        // Usamos una función modular para leer cada nota con su validación
+        nota1 = leerNota(sc, 1);
+        nota2 = leerNota(sc, 2);
+        nota3 = leerNota(sc, 3);
 
         promedio = (nota1 + nota2 + nota3) / 3;
-        
         System.out.println("El promedio es: " + promedio);
+    }
+
+    // Esta es la nueva función que valida el rango requerido por el laboratorio
+    public static int leerNota(Scanner sc, int numero) {
+        int nota;
+        do {
+            System.out.print("Ingrese nota " + numero + " (0-20): ");
+            nota = sc.nextInt();
+        } while (nota < 0 || nota > 20); // Se repite si está fuera de rango
+        return nota;
     }
 }
